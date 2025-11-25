92531c347f61c673b50d6f43e0ec8ded697f50f6
nazarbodak221 <nazak.bodak1993@gmail.com>
2025-11-25 12:40:37 +0200

    Corrected settings and add models

--- test_project/app/models.py
+++ test_project/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.core.validators import MinValueValidator
+
+class TimeEntry(models.Model):
+    project_name=models.CharField(max_length=100)
+    description=models.TextField()
+    hours=models.FloatField(validators=[MinValueValidator(0.1)])
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name="Time entry"
+
+    def __str__(self):
+        return f"{self.project_name} - {self.hours}h"
--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1(!=@5ppl+dxf2w!q@^gzm+hbzx%2is%5g2)54x7kn8d7904!2'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app'
 ]
 
 MIDDLEWARE = [
